f4e96e149eb632e745ffd417c7ce8366f3fa1f7f
aperigno <alexisperignon@live.fr>
2019-10-28 12:21:35 +0100

    Phi 1 DONE

--- src/Solving.c
+++ src/Solving.c
@@ -25,15 +25,37 @@ Z3_ast getNodeVariable(Z3_context ctx, int number, int position, int k, int node
 /**
 * @param numGraph The indice of the graph .
 */
+
+
+// Differ a little from the formula of the rapport but is equivalent 
 Z3_ast getIsPathFormula_PHI_1(Z3_context ctx, Graph graph,unsigned int numGraph, int pathLength){
-	
-	for(int node = 0; node<graph.numNodes;node++){
-		for(int position = 0; position<pathLength;position++){ 
-			//printf("%d : %s, ",node,graph.nodes[node]);
-			//printf("%d : %d : %d : %d \n",numGraph, position, pathLength, node);
-			getNodeVariable(ctx, numGraph, position, pathLength, node);
-		}
+	Z3_ast clause[1000];
+	int indice_clause = 0;
+	Z3_ast Or_clause[1000];
+	int indice_Or_clause = 0;
+	for(int position = 0; position<pathLength;position++){ 
+		for(int u = 0; u<graph.numNodes;u++){
+			for(int v = 0; v<graph.numNodes;v++){
+				if(u!=v){
+						//printf("%d : %s, ",node,graph.nodes[node]);//[DEBUG]
+						//printf("%d : %d : %d : %d \n",numGraph, position, pathLength, node); //[DEBUG]
+						Z3_ast xj = getNodeVariable(ctx, numGraph, position, pathLength, u);
+						Z3_ast xj_1 = getNodeVariable(ctx, numGraph, position+1, pathLength, v);
+						Z3_ast anOtherTab[2] = {xj,xj_1};
+						clause[indice_clause] = Z3_mk_and(ctx,2,anOtherTab);
+						//printf("We have now: %s\n\n",Z3_ast_to_string(ctx,clause[indice_clause])); //[DEBUG]
+						indice_clause++;
+					}
+				}
+			} 
+		Or_clause[indice_Or_clause] = Z3_mk_or(ctx,indice_clause,clause);
+		//printf("OR CLAUSE : \n  %s created.\n",Z3_ast_to_string(ctx,Or_clause[indice_Or_clause])); //[DEBUG]
+		indice_Or_clause++;
+		indice_clause = 0;
 	}
+	Z3_ast And_clause = Z3_mk_and(ctx,indice_Or_clause,Or_clause);
+	printf("AND CLAUSE : \n  %s created.\n",Z3_ast_to_string(ctx,And_clause));
+	return And_clause;
 }
 
 /**
@@ -52,5 +74,25 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
 	for(int i = 0; i<numGraphs;i++){ 
 		Phi_1[i]= getIsPathFormula_PHI_1(ctx,graphs[i],i,pathLength);
 		if(i!=numGraphs-1) printf("********************Next graph : ********************\n");
+		//printf("Phi 1 : %d ", isFormulaSat(ctx,Phi_1[i]));
+		switch (isFormulaSat(ctx,Phi_1[i]))
+        {
+        case Z3_L_FALSE:
+            printf("%s is not satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
+            break;
+
+        case Z3_L_UNDEF:
+                printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
+            break;
+
+        case Z3_L_TRUE:
+                printf("%s \nphi1 is satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
+				printf("***********************************************************\n");
+                //Z3_model model = getModelFromSatFormula(ctx,Phi_1[i]);
+                //printf("Model obtained for %s:\n",Z3_model_to_string(ctx,model));
+
+                break;
+        }
+
 	}
 }
