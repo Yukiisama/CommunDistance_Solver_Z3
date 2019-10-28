59c47131796f7699cd3f3734a460d03a82da85a3
aperigno <alexisperignon@live.fr>
2019-10-28 13:14:20 +0100

    PHI 2 done

--- src/Solving.c
+++ src/Solving.c
@@ -23,17 +23,21 @@ Z3_ast getNodeVariable(Z3_context ctx, int number, int position, int k, int node
 	}
 
 /**
-* @param numGraph The indice of the graph .
+ * @param ctx The solver context.
+ * @param graph The graph
+ * @param numGraph The number of the graph
+ * @param pathLength The length of the path to check.
+ * @return Z3_ast The formula phi 1.
+*  @brief : Differ a little from the formula phi 1 of the rapport but is equivalent , Check if the solution is a path
 */
 
-
-// Differ a little from the formula of the rapport but is equivalent 
 Z3_ast getIsPathFormula_PHI_1(Z3_context ctx, Graph graph,unsigned int numGraph, int pathLength){
+	printf("BEGIN PHI 1 \n");
 	Z3_ast clause[1000];
 	int indice_clause = 0;
 	Z3_ast Or_clause[1000];
 	int indice_Or_clause = 0;
-	for(int position = 0; position<pathLength;position++){ 
+	for(int position = 0; position<pathLength;position++){ //j
 		for(int u = 0; u<graph.numNodes;u++){
 			for(int v = 0; v<graph.numNodes;v++){
 				if(u!=v){
@@ -55,7 +59,64 @@ Z3_ast getIsPathFormula_PHI_1(Z3_context ctx, Graph graph,unsigned int numGraph,
 	}
 	Z3_ast And_clause = Z3_mk_and(ctx,indice_Or_clause,Or_clause);
 	printf("AND CLAUSE : \n  %s created.\n",Z3_ast_to_string(ctx,And_clause));
+	printf("END PHI 1 \n");
 	return And_clause;
+}
+
+
+/**
+ * @param ctx The solver context.
+ * @param graph The graph
+ * @param numGraph The number of the graph
+ * @param pathLength The length of the path to check.
+ * @return Z3_ast The formula phi 2.
+*  @brief : Differ a little from the formula phi 2 of the rapport but is equivalent , Check if the solution is a path
+*/
+
+Z3_ast getIsPathFormula_PHI_2(Z3_context ctx, Graph graph,unsigned int numGraph, int pathLength){
+	printf("BEGIN PHI 2 \n");
+	Z3_ast clause[1000];
+	int indice_clause = 0;
+	Z3_ast Or_clause[1000];
+	int indice_Or_clause = 0;
+
+	for(int position = 0; position<pathLength;position++){ 
+		for(int u = 0; u<graph.numNodes;u++){
+			clause[indice_clause] = getNodeVariable(ctx, numGraph, position, pathLength, u);
+			indice_clause++;
+
+		}
+		Or_clause[indice_Or_clause] = Z3_mk_or(ctx,indice_clause,clause);
+		printf("OR CLAUSE : \n  %s created.\n",Z3_ast_to_string(ctx,Or_clause[indice_Or_clause])); //[DEBUG]
+		indice_Or_clause++;
+		indice_clause = 0;
+	}
+	Z3_ast And_clause = Z3_mk_and(ctx,indice_Or_clause,Or_clause);
+	printf("AND CLAUSE : \n  %s created.\n",Z3_ast_to_string(ctx,And_clause));
+	printf("END PHI 2 \n");
+	return And_clause;
+}
+
+void check_satisfiable(Z3_context ctx,Z3_ast f,const char * str){
+	switch (isFormulaSat(ctx,f))
+        {
+        case Z3_L_FALSE:
+            printf("%s is not satisfiable.\n",Z3_ast_to_string(ctx,f));
+            break;
+
+        case Z3_L_UNDEF:
+                printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,f));
+            break;
+
+        case Z3_L_TRUE:
+                printf("%s \n %s is satisfiable.\n",Z3_ast_to_string(ctx,f),str);
+				printf("***********************************************************\n");
+                //Z3_model model = getModelFromSatFormula(ctx,Phi_1[i]);
+                //printf("Model obtained for %s:\n",Z3_model_to_string(ctx,model));
+
+                break;
+        }
+
 }
 
 /**
@@ -71,28 +132,13 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
 
 	/**********************************Get Sub_Formulas***************************************/
 	Z3_ast Phi_1[numGraphs];
+	Z3_ast Phi_2[numGraphs];
 	for(int i = 0; i<numGraphs;i++){ 
 		Phi_1[i]= getIsPathFormula_PHI_1(ctx,graphs[i],i,pathLength);
+		Phi_2[i]= getIsPathFormula_PHI_2(ctx,graphs[i],i,pathLength);
 		if(i!=numGraphs-1) printf("********************Next graph : ********************\n");
-		//printf("Phi 1 : %d ", isFormulaSat(ctx,Phi_1[i]));
-		switch (isFormulaSat(ctx,Phi_1[i]))
-        {
-        case Z3_L_FALSE:
-            printf("%s is not satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
-            break;
-
-        case Z3_L_UNDEF:
-                printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
-            break;
-
-        case Z3_L_TRUE:
-                printf("%s \nphi1 is satisfiable.\n",Z3_ast_to_string(ctx,Phi_1[i]));
-				printf("***********************************************************\n");
-                //Z3_model model = getModelFromSatFormula(ctx,Phi_1[i]);
-                //printf("Model obtained for %s:\n",Z3_model_to_string(ctx,model));
-
-                break;
-        }
-
+		check_satisfiable(ctx, Phi_1[i] ,"PHI 1");
+		check_satisfiable(ctx, Phi_2[i] ,"PHI 2");
+		
 	}
 }
